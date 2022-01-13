5fdc9a71b0c5b33812addbc1cb838625bd67ec83
osmolenko <osmolenko.s@gmail.com>
2022-01-14 00:11:22 +0200

    feat: add react-input-mask

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,8 +3,10 @@ import styles from './Button.module.scss'
 
 const Button = React.forwardRef(function Button(inProps, ref) {
   const { variant, children } = inProps
+  const props = () => ({ ...inProps, className: '' })
+
   return (
-    <button className={styles[variant]} ref={ref} {...inProps}>
+    <button className={styles[variant]} ref={ref} {...props}>
       {children}
     </button>
   )
--- components/TextInput.jsx
+++ components/TextInput.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import styles from './TextInput.module.scss'
 import Eye from '../icons/Eye'
 import EyeOff from '../icons/EyeOff'
+import InputMask from 'react-input-mask'
 
 const TextInput = React.forwardRef(function TextInput(inProps, ref) {
-  const { type, name, labelText } = inProps
+  const { type, name, labelText, className } = inProps
   const [showPassword, setShowPassword] = useState(true)
   const showEye = type === 'password' && showPassword
+
+  const props = () => ({ ...inProps, className: '' })
 
   const PasswordToggle = () => {
     return showEye ? (
@@ -17,21 +20,32 @@ const TextInput = React.forwardRef(function TextInput(inProps, ref) {
   }
 
   return (
-    <>
+    <div className={className}>
       <label className={styles.label} htmlFor={name}>
         {labelText}
       </label>
       <div className={styles.container}>
-        <input
-          name={name}
-          id={name}
-          {...inProps}
-          ref={ref}
-          type={showEye ? 'password' : 'text'}
-        />
+        {type === 'tel' ? (
+          <InputMask
+            mask="+999 99 999 99 99"
+            name={name}
+            id={name}
+            {...props()}
+            ref={ref}
+          />
+        ) : (
+          <input
+            name={name}
+            id={name}
+            {...props()}
+            ref={ref}
+            type={showEye ? 'password' : 'text'}
+          />
+        )}
+
         {type === 'password' ? <PasswordToggle /> : ''}
       </div>
-    </>
+    </div>
   )
 })
 
