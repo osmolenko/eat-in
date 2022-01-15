f1edb3a536f5a432f065794b075aa782ce383c84
osmolenko <osmolenko.s@gmail.com>
2022-01-15 13:38:15 +0200

    feat: simple pages icons

---
+++ icons/404.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Icon404 = () => {
+  return (
+    <svg
+      width={128}
+      height={128}
+      viewBox="0 0 128 128"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M63.9998 117.333C93.455 117.333 117.333 93.4552 117.333 64C117.333 34.5448 93.455 10.6667 63.9998 10.6667C34.5447 10.6667 10.6665 34.5448 10.6665 64C10.6665 93.4552 34.5447 117.333 63.9998 117.333Z"
+        stroke="black"
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M64 42.6667V64"
+        stroke="black"
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M64 85.3333H64.0533"
+        stroke="black"
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Icon404
---
+++ icons/Error.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const IconError = () => {
+  return (
+    <svg
+      width={128}
+      height={128}
+      viewBox="0 0 128 128"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M64.0001 117.333C93.4553 117.333 117.333 93.4552 117.333 64C117.333 34.5448 93.4553 10.6667 64.0001 10.6667C34.5449 10.6667 10.6667 34.5448 10.6667 64C10.6667 93.4552 34.5449 117.333 64.0001 117.333Z"
+        stroke="black"
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M80 48L48 80"
+        stroke="black"
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M48 48L80 80"
+        stroke="black"
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default IconError
