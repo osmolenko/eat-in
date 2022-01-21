d34d13d7c343a4511a37855f3952928defbadd30
osmolenko <osmolenko.s@gmail.com>
2022-01-21 14:48:46 +0200

    feat: global loader

--- containers/AppContainer.jsx
+++ containers/AppContainer.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './AppContainer.module.scss'
 import Link from 'next/link'
 import Shop from '../icons/Shop'
 import Cart from '../icons/Cart'
 import Profile from '../icons/Profile'
+import { useRouter } from 'next/router'
+import Loader from '../components/basic/Loader'
 
 const AppContainer = ({ children }) => {
+  const router = useRouter()
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setLoading(true)
+    const handleComplete = (url) => url === router.asPath && setLoading(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+
   return (
     <>
-      <main className={styles.content}>{children}</main>
+      {loading && <Loader />}
+
+      <main
+        style={loading ? { overflow: 'hidden', height: '100vh' } : {}}
+        className={styles.content}
+      >
+        {children}
+      </main>
       <footer>
         <nav className={styles.footer}>
           <ul className={styles.footer__container}>
@@ -29,6 +57,7 @@ const AppContainer = ({ children }) => {
                 </a>
               </Link>
             </li>
+
             <li>
               <Link href="/profile">
                 <a className={styles.footer__link}>
