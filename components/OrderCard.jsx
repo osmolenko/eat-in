89469b6aabedb4dd4f6991318a3f52555ccad1fe
osmolenko <osmolenko.s@gmail.com>
2022-01-21 16:45:43 +0200

    feat: order templating added

--- components/OrderCard.jsx
+++ components/OrderCard.jsx
@@ -1,63 +1,78 @@
 import React, { useState } from 'react'
 import styles from './OrderCard.module.scss'
 import Chevron from '../icons/Chevron'
+import { paymentMethods, timeIntervals } from '../constants'
+import { findInArray } from '../utils/arrays'
 
-const OrderCard = () => {
+const OrderCard = ({
+  order: {
+    pid,
+    createdAt,
+    summary,
+    status,
+    products,
+    city,
+    address,
+    apt,
+    payment,
+    time,
+  },
+}) => {
   const [expanded, setExpanded] = useState(false)
 
   return (
     <div className={styles.container}>
-      <span className={styles.container__number}>Заказ 321451</span>
-      <span className={styles.container__sum}>1797₴</span>
-      <span className={styles.container__date}>12 января 2022 15:45</span>
-      <span className={styles.container__status}>В обработке</span>
+      <span className={styles.container__number}>Заказ {pid}</span>
+      <span className={styles.container__sum}>{summary}₴</span>
+      <span className={styles.container__date}>
+        {new Date(createdAt).toLocaleString()}
+      </span>
+      <span className={styles.container__status}>
+        {status === 0 ? 'В обработке' : `Выполнен`}
+      </span>
 
       <div className={styles.container__more}>
         {expanded ? (
           <>
             <hr />
 
-            <div className={styles.container__more__item}>
-              <div className={styles.container__more__item__head}>
-                <span>Набор SlimFit</span>
-                <span>1797₴</span>
-              </div>
-              <div className={styles.container__more__item__product}>
-                <span>Глазунья, булгур, льяныне флаксы с хумусом</span>
-                <span>3 шт.</span>
-              </div>
-              <div className={styles.container__more__item__product}>
-                <span>Ризотто с овощами</span>
-                <span>3 шт.</span>
-              </div>
-            </div>
+            {products &&
+              products.map((product) => (
+                <ul key={product._id} className={styles.container__more__item}>
+                  <li className={styles.container__more__item__head}>
+                    <span>
+                      {product.quantity} x Набор {product.product.name}
+                    </span>
+                    <span>{product.product.price}₴</span>
+                  </li>
+                </ul>
+              ))}
 
             <hr />
 
-            <div className={styles.container__more__info}>
-              <span>
-                <b>Адрес:</b> г.Киев, ул. Крещатик, 36, кв.9
-              </span>
-              <span>
-                <b>Оплата:</b> При получении
-              </span>
-              <span>
-                <b>Время получения:</b> 9:00 - 10:00
-              </span>
-              <span>
+            <ul className={styles.container__more__info}>
+              <li>
+                <b>Адрес:</b> {city}, {address}, кв.{apt}
+              </li>
+              <li>
+                <b>Оплата:</b> {findInArray(paymentMethods, payment)}
+              </li>
+              <li>
+                <b>Время получения:</b> {findInArray(timeIntervals, time)}
+              </li>
+              <li>
                 <b>Статус заказа:</b> Выполнен
-              </span>
-            </div>
+              </li>
+            </ul>
           </>
         ) : (
           ''
         )}
-
         <button
           className={styles.container__more__expand}
           onClick={() => setExpanded(!expanded)}
         >
-          <Chevron />
+          <Chevron style={expanded ? { transform: 'rotate(180deg)' } : {}} />
           {expanded ? 'Скрыть' : 'Узнать больше'}
         </button>
       </div>
